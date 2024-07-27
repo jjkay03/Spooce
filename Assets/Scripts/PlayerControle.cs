cdf428907ec65e506ec044c51d3a9cb1b502377c
jjkay03 <61110962+jjkay03@users.noreply.github.com>
2024-07-27 17:08:59 +0300

    ☀️ First commit

---
+++ Assets/Scripts/PlayerControle.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class PlayerControle : MonoBehaviour {
+    /* -------------------------------- Variables ------------------------------- */
+
+    [Header("Keybinds")]
+    public KeyCode[] moveFrontKey = { KeyCode.W };
+    public KeyCode[] moveBackKey = { KeyCode.S };
+    public KeyCode[] moveLeftKey = { KeyCode.A };
+    public KeyCode[] moveRightKey = { KeyCode.D };
+    public KeyCode[] rotateLeftKey = { KeyCode.Q };
+    public KeyCode[] rotateRightKey = { KeyCode.E };
+
+
+    /* ------------------------------ Main Classes ------------------------------ */
+    
+    // Start is called before the first frame update
+    void Start() {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+        
+    }
+    
+
+    /* --------------------------------- Classes -------------------------------- */
+}
